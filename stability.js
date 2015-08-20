924798459a3ad53f1e29b6c69da50b4908fa79d6
Kyriosli <lz3259@gmail.com>
2015-08-20 15:28:33 +0800

    save

--- stability.js
+++ stability.js
@@ -1,27 +1,27 @@
 var cp = require('child_process');
 
 if(process.argv[2] !== 'worker') {
-	var workers = [], spawned = 0;
-	for(var i = 0; i < 10; i++) {
-		spawned++;
-		var worker = workers[i] = cp.spawn(process.execPath, [__filename, 'worker'], {stdio: 'inherit'});
-	}
-	process.on('SIGINT', function () {
-		for(var i = 0; i < 10; i++) {
-			workers[i].kill('SIGINT');
-		}
-		clearInterval(pid);
-		console.log(spawned + ' children spawned');
-	});
+    var workers = [], spawned = 0;
+    for(var i = 0; i < 10; i++) {
+        spawned++;
+        var worker = workers[i] = cp.spawn(process.execPath, [__filename, 'worker'], {stdio: 'inherit'});
+    }
+    process.on('SIGINT', function () {
+        for(var i = 0; i < 10; i++) {
+            workers[i].kill('SIGINT');
+        }
+        clearInterval(pid);
+        console.log(spawned + ' children spawned');
+    });
 
-	var pid = setInterval(function() {
-		var i = Math.random() * 10 | 0;
-		workers[i].kill('SIGKILL');
-		spawned++;
-		var worker = workers[i] = cp.spawn(process.execPath, [__filename, 'worker'], {stdio: 'inherit'});
-	}, 100);
+    var pid = setInterval(function() {
+        var i = Math.random() * 10 | 0;
+        workers[i].kill('SIGKILL');
+        spawned++;
+        var worker = workers[i] = cp.spawn(process.execPath, [__filename, 'worker'], {stdio: 'inherit'});
+    }, 100);
 
-	return;
+    return;
 }
 
 var n = 0, stopped = false;
@@ -30,25 +30,25 @@ var obj = new binding.Cache("test", 512<<10, binding.SIZE_1K);
 
 
 process.on('SIGINT', function () {
-	console.log(process.pid, n);
-	stopped = true;
+    console.log(process.pid, n);
+    stopped = true;
 });
 
 function sched() {
-	n++;
-	for(var i = 0; i < 1000; i++) {
-		switch(Math.random() * 10 | 0) {
-			case 0: obj.foo = !obj.foo; break;
-			case 1: obj.foo = Date.now() + Math.random(); break;
-			case 2: obj.foo = process.env; break;
-			case 3: delete obj.foo; break;
-			case 4: Object.keys(obj); break;
-			case 5: obj.foo = null; break;
-			case 6: 'foo' in obj; break;
-			default: obj.foo; break;
-		}
-	}
-	stopped || setTimeout(sched, 0);
+    n++;
+    for(var i = 0; i < 1000; i++) {
+        switch(Math.random() * 10 | 0) {
+            case 0: obj.foo = !obj.foo; break;
+            case 1: obj.foo = Date.now() + Math.random(); break;
+            case 2: obj.foo = process.env; break;
+            case 3: delete obj.foo; break;
+            case 4: Object.keys(obj); break;
+            case 5: obj.foo = null; break;
+            case 6: 'foo' in obj; break;
+            default: obj.foo; break;
+        }
+    }
+    stopped || setTimeout(sched, 0);
 }
 
 sched();
---
+++ stability2.js
@@ -0,0 +1,19 @@
+var binding = require('./index.js');
+var obj = new binding.Cache("test", 512<<10, binding.SIZE_1K);
+
+var key = 'foo';
+
+for(var i = 0; ; i++) {
+    switch(Math.random() * 10 | 0) {
+        case 0: obj[key] = !obj[key]; break;
+        case 1: obj[key] = Date.now() + Math.random(); break;
+        case 2: obj[key] = process.env; break;
+        case 3: delete obj[key]; break;
+        case 4: Object.keys(obj); break;
+        case 5: obj[key] = null; break;
+        case 6: key in obj; break;
+        case 7: key = 'foo' + (i & 7);
+        default: obj[key]; break;
+    }
+    (i & 4095) || console.log(i);
+}
